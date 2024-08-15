e7ecf5f8f92a0bc03464f8843bed60663db452b8
Muhammed Nibeen <nibiniz339@gmail.com>
2024-08-16 00:14:57 +0530

    Update after hosting

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -1,7 +1,7 @@
 export const enviroment = {
   production : true,
-  baseUrl : 'http://localhost:3000'
-  // baseUrl : 'https://speculo.shop'
+  // baseUrl : 'http://localhost:3000'
+  baseUrl : 'https://speculo.shop'
 }
 
 export const RZPKEYID="rzp_test_jHotBnTKoHrYXJ"
